02fbcae65656b223a50fc59b3487d2594144c971
mr-lvheng <568505455@qq.com>
2021-06-18 18:08:34 +0800

    第一次提交

---
+++ Welcome.java
@@ -0,0 +1,8 @@
+public class Welcome {
+    public static void main(String[] args) {
+        String[] greeting =new String[3];
+        greeting[0]="Welcome to Core Java";
+        greeting[1]="by Cay Horstmann";
+        greeting[2]="and Gary Cornell";
+    }
+}
